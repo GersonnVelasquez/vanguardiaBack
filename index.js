9857ea48ae292bc50dda2e5ed2502b295cbbd3af
Gersonn Velasquez <yersonn.velasquez@gmail.com>
2021-09-17 13:29:23 -0600

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var mongoose = require("mongoose");
 var app = require("./app");
-var port = 3900;
+var port = process.env.PORT || 3000;
 require("dotenv").config();
 
 // mongoose.set('useFindAndModify', false);
